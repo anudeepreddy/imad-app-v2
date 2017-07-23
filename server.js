b6da0ad7656531859d58c4877c7a23bcad5d5a31
anudeepreddy <anudeepc85@gmail.com>
2017-07-23 18:52:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,9 +21,7 @@ app.get('/article-two',function(req,res){
     res.send('aritcle two');
 });
 
-app.get('/article-one',function(req,res){
-  //  res.sendFile(path.join(__dirname),'ui','article-one.html'));
-});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
